14c620098583e3d3a93c5c8dec7a82e2b0991326
Ricardo Monteiro <rcmonteiro@gmail.com>
2024-04-14 10:33:29 -0300

    Fix: Add Suspense boundaries when using useSearchParams()

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { Skeleton } from '@/components/skeleton'
+import { Suspense } from 'react'
+import { CurrentSearch } from './current-search'
 
 export default function Loading() {
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">****</span>
-      </p>
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
       <div className="grid grid-cols-3 gap-6">
         {Array.from({ length: 6 }).map((_, index) => {
           return <Skeleton key={index} className="aspect-square" />
